48bfa120df8ad4c98559970f04131f9672292711
MrArcano <salvatore-dario@hotmail.it>
2023-11-23 16:55:43 +0100

    [add]: migration comic

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,15 @@
+<header>
+    <nav class="navbar bg-primary border-bottom border-body" data-bs-theme="dark">
+        <ul class="nav m-auto fw-bold">
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Comics list</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Add new comics</a>
+            </li>
+        </ul>
+    </nav>
+</header>
