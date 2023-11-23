12d7ebbea1a271d75043b1e85eed13584bf80670
MrArcano <salvatore-dario@hotmail.it>
2023-11-23 19:29:43 +0100

    [add]: sluf static function model Comic

--- app/Models/Comic.php
+++ app/Models/Comic.php
@@ -4,8 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Comic extends Model
 {
     use HasFactory;
+    private static function generateSlug($string){
+        // genero il mio slug
+        $slug = Str::slug($string, '-');
+        $origina_slug = $slug;
+
+        // controllo se nel mio db esiste già
+        $exist = Comic::where('slug',$slug)->first();
+        $counter=1;
+        while($exist){
+            $slug = $origina_slug .'-'. $counter;
+            $counter++;
+            $exist = Comic::where('slug',$slug)->first();
+        }
+
+        return $slug;
+    }
 }
